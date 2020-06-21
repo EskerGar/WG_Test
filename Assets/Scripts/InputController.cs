3f5cb5c57b56e426dbcfed591faeb7554f1cd597
Esker_Gar <lobachevartem@gmail.com>
2020-06-21 21:44:57 +0300

    ShipMove

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField] private KeyCode speedUp = KeyCode.UpArrow;
+    [SerializeField] private KeyCode speedDown = KeyCode.DownArrow;
+    private MovementController _move;
+
+    public event Action<float> OnSpeedUp;
+    public event Action<float> OnSpeedDown; 
+    private float Rotation => Input.GetAxis("Horizontal");
+
+
+    private void Start()
+    {
+        _move = GetComponent<MovementController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown(speedUp))
+            OnSpeedUp?.Invoke(_move.GetSpeedStep);
+        else if (Input.GetKeyDown(speedDown))
+            OnSpeedDown?.Invoke(_move.GetSpeedStep);
+        _move.Move();
+        _move.Rotate(Rotation);
+    }
+    
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] [Min(0)] private float moveSpeed;
+    [SerializeField] [Min(0)] private float rotateSpeed;
+    [SerializeField] private float speedStep;
+    [SerializeField] private float minMoveSpeed;
+    [SerializeField] private float maxMoveSpeed;
+
+    public float GetSpeedStep => speedStep;
+
+    private Rigidbody2D _rb;
+    private InputController _input;
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _input = GetComponent<InputController>();
+        _input.OnSpeedUp += ChangeSpeed;
+        _input.OnSpeedDown += ChangeSpeed;
+    }
+
+
+    private void ChangeSpeed(float value)
+    {
+        moveSpeed += value;
+            if (moveSpeed > maxMoveSpeed)
+                moveSpeed = maxMoveSpeed;
+            else if (moveSpeed < minMoveSpeed)
+                moveSpeed = minMoveSpeed;
+    }
+    
+    public void Move() => _rb.velocity = transform.up * (moveSpeed * Time.deltaTime);
+
+    public void Rotate(float moveVector) => _rb.rotation += ( rotateSpeed * moveVector * Time.deltaTime);
+}
